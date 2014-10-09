a891203c358b5f7cf2924f711f0625f8087b0b90
mfantonidas <7225888>
2014-10-09 23:57:07 +0800

    3

--- CoreSYS/settings.py
+++ CoreSYS/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap_toolkit',
+    'bootstrap3',
     'crispy_forms',
     'upFile',
     'xadmin',
